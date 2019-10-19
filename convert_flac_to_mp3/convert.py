ea3fe49753d958f32f082f244912d5f4b2102780
Somshubra Majumdar <titu1994@gmail.com>
2019-10-19 16:05:32 -0500

    Add flac to mp3 converter

---
+++ convert_flac_to_mp3/convert.py
@@ -0,0 +1,13 @@
+import os
+import glob
+from subprocess import call
+
+flac_files = glob.glob("*.flac")
+
+for fp in flac_files:
+    new_name = os.path.splitext(fp)[0] + ".mp3"
+
+    call(["ffmpeg", "-i", fp, "-q:a", "0",new_name])
+    print("Converted file : ", new_name)
+
+print("Finished converting all files !")
