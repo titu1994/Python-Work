f5f79d831f4f1eb91ba5adbecb207d4eeda3a420
Somshubra Majumdar <titu1994@gmail.com>
2020-11-20 18:27:29 -0800

    Update

---
+++ medicine/merge_pdfs.py
@@ -0,0 +1,43 @@
+import os
+import glob
+import argparse
+
+from PyPDF2 import PdfFileMerger
+
+
+parser = argparse.ArgumentParser('Python pdf file merger')
+
+parser.add_argument('-f', dest='filepaths', type=str, nargs='+', default=None, required=False, help='List of file paths')
+parser.add_argument('-d', dest='directory', type=str, default=None, required=False, help='Directory of file paths')
+parser.add_argument('-o', dest='output', type=str, default=None, required=False, help='Output file path')
+
+args = parser.parse_args()
+
+filepaths = args.filepaths
+directory = args.directory
+output_dir = args.output
+
+if (filepaths is None or len(filepaths) == 0) and (directory is None):
+    raise FileNotFoundError("No file provided!")
+
+if filepaths is None:
+    filepaths = sorted(list(glob.glob(os.path.join(directory, "*.pdf"))))
+
+if output_dir is None:
+    output_dir = os.getcwd()
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir, exist_ok=True)
+
+merger = PdfFileMerger()
+
+for pdf in filepaths:
+    merger.append(pdf)
+
+
+output_dir = os.path.abspath(output_dir)
+output_filepath = os.path.join(output_dir, 'result.pdf')
+merger.write(output_filepath)
+merger.close()
+
+print(f"Results written to path : {output_filepath}")
