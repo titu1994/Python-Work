bb2da2138f9763c4b967adbbb91a0ffcba360d8f
Somshubra Majumdar <titu1994@gmail.com>
2020-12-08 21:30:15 -0800

    Update

--- medicine/medicine.py
+++ medicine/medicine.py
@@ -1,8 +1,8 @@
 import numpy as np
 import datetime
 
-begin_date = datetime.date(2020, 1, 1)
-end_date = datetime.date(2021, 3, 1)
+begin_date = datetime.date(2021, 1, 1)
+end_date = datetime.date(2022, 4, 1)
 delta = end_date - begin_date
 day_count = delta.days
 
@@ -26,15 +26,17 @@ def count_med_strips(name, per_day, per_strip, verbose=True):
 
 def count_med_strips_with_available_count(name, per_day, per_strip, available_count, verbose=True):
     total_count, number_of_strips = count_med_strips(name, per_day, per_strip, verbose=False)
-    total_count -= available_count
-    number_of_strips = total_count // per_strip + 1
+    number_of_strips = (total_count - available_count) // per_strip + 1
 
     currently_available_strips = available_count // per_strip
 
     if verbose:
-        print('Medicine {:15s}: Count = {:7.2f} ({:0.2f} per day) | Currently Available Strips = {:4.2f} | '
-              'Number of strips to buy = {:3.0f}'.format(
-                name, total_count, per_day, currently_available_strips, number_of_strips
+        print('Medicine {:15s}: Count = {:7.2f} ({:0.2f} per day) | '
+              'Currently Available Strips = {:4.2f} (count={:4.2f}) | '
+              'Number of strips to buy = {:3.0f} (count~={:4.0f})'.format(
+                name, total_count, per_day,
+                currently_available_strips, available_count,
+                number_of_strips, total_count - available_count
             ))
 
     return total_count, number_of_strips
@@ -53,8 +55,8 @@ def count_med_strips_with_available_count(name, per_day, per_strip, available_co
 
 """ Count mediciation with available accounted for """
 count_med_strips_with_available_count(name='Levipil 500', per_day=2, per_strip=10, available_count=0)
-count_med_strips_with_available_count(name='Cardiace 5', per_day=3, per_strip=10, available_count=0)
-count_med_strips_with_available_count(name='Azoran', per_day=3, per_strip=10, available_count=0)
+count_med_strips_with_available_count(name='Ramipril', per_day=3, per_strip=15, available_count=0)
+# count_med_strips_with_available_count(name='Azoran', per_day=3, per_strip=10, available_count=0)
 count_med_strips_with_available_count(name='HCQS 200', per_day=2, per_strip=15, available_count=0)
 count_med_strips_with_available_count(name='Omnacortil 2.5', per_day=2, per_strip=10, available_count=0)
 count_med_strips_with_available_count(name='Ecosporin 75', per_day=1, per_strip=10, available_count=0)
